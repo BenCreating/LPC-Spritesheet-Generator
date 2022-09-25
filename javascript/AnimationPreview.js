f8880228987108de52fb83d5cb52067fea60bee3
Kyle Smith <kyle@smithsrus.com>
2022-09-24 22:11:08 -0500

    Add JSDoc to AnimationPreview

--- javascript/AnimationPreview.js
+++ javascript/AnimationPreview.js
@@ -1,5 +1,15 @@
 import animations from "./animations.js"
 
+/**
+ * Custom element to preview an animation. Insert into the page with:
+ *
+ * <animation-preview></animation-preview>
+ *
+ * Then, get the element with JavaScript to set a source and call start().
+ *
+ * This class makes use of private fields (variables starting with a "#") to
+ * prevent access to internal state.
+ */
 class AnimationPreview extends HTMLElement {
   constructor() {
     super()
@@ -7,17 +17,13 @@ class AnimationPreview extends HTMLElement {
     this.attachShadow({ mode: 'open' })
 
     const wrapper = document.createElement('div')
-    wrapper.part = 'base'
 
     const selectAnimationWrapper = document.createElement('div')
-    selectAnimationWrapper.part = 'select-base'
 
     const label = document.createElement('span')
     label.textContent = 'Preview'
-    label.part = 'select-label'
 
     const select = document.createElement('select')
-    select.part = 'animation-select'
 
     animations.forEach(animation => {
       const option = document.createElement('option')
@@ -31,6 +37,18 @@ class AnimationPreview extends HTMLElement {
 
     selectAnimationWrapper.append(label, select)
     wrapper.append(selectAnimationWrapper, this.#canvas)
+
+    // Normally, global page CSS can't target these elements, because they are
+    // in a shadow root. Setting a "part" allows targeting them with the
+    // ::part() selector. For example:
+    //
+    // animation-preview::part(select-label) {
+    //   background-color: red;
+    // }
+    wrapper.part = 'base'
+    selectAnimationWrapper.part = 'select-base'
+    label.part = 'select-label'
+    select.part = 'animation-select'
     this.#canvas.part = 'animation'
 
     this.shadowRoot.append(wrapper)
@@ -40,27 +58,46 @@ class AnimationPreview extends HTMLElement {
   #canvas = document.createElement('canvas')
   #ctx = this.#canvas.getContext('2d')
 
+  /**
+   * The pending requestAnimationFrame id. Used by stop() to cancel the
+   * animation frame.
+   *
+   * @type {number}
+   */
   #animationLoopId = undefined
-  #animationStart = undefined
-  #source = undefined
-  #shadowRoot = undefined
 
-  #currentFrameIndex = 0
-  #framesPerSecond = 8
+  /**
+   * When the animation was started. Used to calculate the current frame.
+   *
+   * @type {DOMHighResTimeStamp}
+   */
+  #animationStart = undefined
+
+  /**
+   * The image or canvas element for the full spritesheet. Set this before
+   * calling start().
+   *
+   * @type {CanvasImageSource}
+   */
+  source = undefined
+
+  #currentFrameIndex = 0 // The index of the currently displayed frame
+  #framesPerSecond = 8 // Default value, individual animations can set a different frame rate
   #numberOfDirections = 4 // The number of directions a character can face in
 
-  animationLength = 0
-  frameSize = 64
+  animationLength = 0 // This is calculated from the selected animation
+  frameSize = 64 // Public variable to allow for future support of oversized animations
 
-  get source() { return this.#source }
-  set source(newSource) {
-    this.#source = newSource
-  }
-
+  /**
+   * @return {object} The currently selected animation
+   */
   get animation() {
     return animations.find(animation => animation.name === this.#selectedAnimation)
   }
 
+  /**
+   * Start the animation
+   */
   start() {
     this.animationLength = this.animation.width / this.frameSize
     this.#canvas.width = this.frameSize * this.#numberOfDirections
@@ -70,17 +107,26 @@ class AnimationPreview extends HTMLElement {
     this.#animationLoopId = requestAnimationFrame(this.#animate)
   }
 
+  /**
+   * Stop the animation
+   */
   stop() {
     cancelAnimationFrame(this.#animationLoopId)
     this.#animationLoopId = undefined
   }
 
+  /**
+   * Reset the animation, used whenever the selected animation is changed
+   */
   restart() {
     this.stop()
     this.start()
   }
 
   /**
+   * Callback for the requestAnimationFrame loop. Displays the appropriate frame
+   * based on the time since the animation started and the frames per second.
+   *
    * @param {DOMHighResTimeStamp} timestamp
    */
   #animate = timestamp => {
@@ -97,6 +143,12 @@ class AnimationPreview extends HTMLElement {
     this.#currentFrameIndex = nextFrameIndex
   }
 
+  /**
+   * Draw the specified frame of the current animation, for all facing
+   * directions.
+   *
+   * @param {number} frameIndex
+   */
   #draw(frameIndex) {
     this.#ctx.clearRect(0, 0, this.#canvas.width, this.#canvas.height)
 
@@ -114,6 +166,11 @@ class AnimationPreview extends HTMLElement {
     }
   }
 
+  /**
+   * Handle user selection of which animation
+   *
+   * @param {Event} event
+   */
   #updateSelectedAnimation = (event) => {
     this.#selectedAnimation = event.target.value
 
